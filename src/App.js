24c1b6358526e9584f149346f625769870bbb9cc
Malachi Ivey <zarekbusiness@gmail.com>
2019-05-05 20:45:46 -0400

    components/Form.js + readme update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Form from './components/Form';
+
+const API_KEY = '6758a76f591881b7ab6373393990d59b'
+
 class App extends Component {
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();    
+    const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
+
+    const data = await api_call.json();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
